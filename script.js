5705d6606e9a0dd81050d1a7fbf89d288ac478ef
RachellVillarreal <teranrachellkatiuska@gmail.com>
2024-01-23 16:58:18 +0100

    cambio de color en las cards y borde de la cards, también e añadido el párrafo y la implementación de la seach bar

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Dimitri & Kora MICHI MIAU")
+
 const jsonFile = 'db.json';
+let eventos;
 
 fetch(jsonFile)
 .then(response => response.json())
@@ -10,11 +12,13 @@ fetch(jsonFile)
       const card = document.createElement('div');
       card.classList.add('card');
 
+      eventos = data.eventos
+
       card.innerHTML = `
       <img src="${evento.image}" alt="${evento.title}">
         <h3>${evento.title}</h3>
         <p>${evento.description}</p>
-        <p>${evento.category}</p>
+        <p>${evento.price}</p>
       `;
 
       eventosContainer.appendChild(card);
@@ -24,3 +28,34 @@ fetch(jsonFile)
     console.log(data);
 })
 .catch(error => console.error('Error fetching JSON:', error));
+
+
+//siguiente codigo es el buscador//
+
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('input', function() {
+  const searchterm = this.value.toLowerCase();
+
+  console.log(searchterm);
+
+  const eventosFiltrados = eventos.filter(evento => {
+    return evento.title.toLowerCase().includes(searchterm);
+  });
+
+  eventosContainer.innerHTML = '';
+
+  eventosFiltrados.forEach(evento => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML =`
+    <img src="${evento.image}" alt="${evento.title}">
+    <h3>${evento.title}</h3>
+    <p>${evento.description}</p>
+    <p>${evento.price}</p>
+`;
+
+eventosContainer.appendChild(card);
+  });
+});
